bef585aa66a1b6297acf1eff8efa5b4fe70ebbd9
Özgün Arslantürk <ozgunars@icloud.com>
2021-04-08 15:16:28 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,13 @@
-#Explain your work
+#kullanıcı 
 
-#Question 1
-for x in range(a):
-	print(a)
+kullanıcıadı=str("özgün")
+şifre=(123456)
+a=str(input("kullanıcı adınızı giriniz:"))
+if ad==a:
+    b=int(input("şifrenizi giriniz:"))
+    if b==şifre:
+        print("Doğru.")
+    else:
+        print("şifre yanlış")
+else:
+    print("kullanıcı adı yanlış.")
