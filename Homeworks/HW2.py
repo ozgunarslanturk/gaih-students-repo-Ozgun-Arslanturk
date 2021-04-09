446c10a4ea28012557421440cea53ca3b426cbd7
Özgün Arslantürk <ozgunars@icloud.com>
2021-04-09 14:01:37 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -3,7 +3,7 @@
 kullanıcıadı=str("özgün")
 şifre=(123456)
 a=str(input("kullanıcı adınızı giriniz:"))
-if ad==a:
+if kullanıcıadı==a:
     b=int(input("şifrenizi giriniz:"))
     if b==şifre:
         print("Doğru.")
